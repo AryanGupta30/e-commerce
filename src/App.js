20f8ef35ea02297c1fe5bca9a1dc5e0c8e0a66d8
Aryan Gupta <30aryang@gmail.com>
2024-06-26 10:58:17 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Footer from './customer/components/Footer/Footer';
+import Product from './customer/components/Products/Product';
 import Navigation from './customer/components/navigation/navigation';
 import HomePage from './customer/pages/HomePage/HomePage';
 function App() {
@@ -6,9 +8,10 @@ function App() {
     <div className="">
       <Navigation/>
       <div>
-        <HomePage/>
-        hello
+        {/* <HomePage/> */}
+        <Product/>
       </div>
+      <Footer/>
     </div>
   );
 }
---
+++ src/customer/components/Products/ProductCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div className='productCard w-[15rem] m-3 transition-all cursor-pointer'>
+        <div className='h-[20rem]'>
+            <img className='h-full w-full object-cover object-left-top' src="https://ix-marketing.imgix.net/imgix-hero-ai-genfill-4.gif?auto=format,compress?w=1104" alt="product" />
+        </div>
+        <div className='textPart bg-white p-3'>
+            <div>
+                <p className='font-bold opacity-60'>UniversalOutfit</p>
+                <p>Casual White Top</p>
+            </div>
+            <div className='flex items-center space-x-2'>
+                <p className='font-semibold'>$199</p>
+                <p className='line-through opacity-50'>$1999</p>
+                <p className='text-green-600 font-semibold'>70% off</p>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard
